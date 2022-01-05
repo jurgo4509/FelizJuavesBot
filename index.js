2583133eb22a280ac6c269f34021339e13fdf00a
Jurģis Ieviņš <93622896+jurgo4509@users.noreply.github.com>
2022-01-05 13:02:48 +0000

    changed res.send() from 'amogus!' to 'sus2!'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('amogus!'));
+app.get('/', (req, res) => res.send('sus2!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
